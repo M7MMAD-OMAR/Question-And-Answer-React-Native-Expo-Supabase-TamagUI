e951e57a822b57b79ce4980e623b9250e9550b4a
M7MMAD-OMAR <m7mmad.omar0@gmail.com>
2024-08-21 22:46:52 +0300

    Refactor component to display a list of questions using FlatList. 🚀

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,16 +1,34 @@
-import {View} from "tamagui";
+import {ListItem, Text} from "tamagui";
+import {FlatList} from "react-native";
+import {useState} from "react";
 
-const question = [
+interface Question {
+    id: number
+    title: string
+}
+
+const questions = [
     {'id': 1, 'title': 'How are you?'},
     {'id': 2, 'title': 'How older?'},
     {'id': 3, 'title': 'Am i on the right path?'},
 ]
 
 const HomePage = () => {
-    return (
-        <View>
+    const [data, setData] = useState(questions)
 
-        </View>
+
+    return (
+        <FlatList
+            data={data}
+            contentContainerStyle={{padding: 20}}
+            keyExtractor={(item) => item.id.toString()}
+            renderItem={({item, index}: { item: Question, index: number }) => {
+                return (
+                    <ListItem my="$2" key={item.id}>
+                        <Text>{index + 1}. {item.title}</Text>
+                    </ListItem>
+                );
+            }}/>
     );
 };
 
